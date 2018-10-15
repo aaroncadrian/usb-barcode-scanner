b039d1e6c3c2b77a504e483dbe69ed025a8de6de
Aaron Adrian <aaroncadrian@gmail.com>
2018-10-14 20:27:45 -0500

    Export HidMap

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export { BarcodeScanner } from './scanner/barcode-scanner';
-export { UsbScanner } from './scanner/usb-scanner';
-export { OnDataScanned } from './scanner/usb-barcode-scanner-types';
+export { BarcodeScanner } from "./scanner/barcode-scanner";
+export { UsbScanner } from "./scanner/usb-scanner";
+export { OnDataScanned } from "./scanner/usb-barcode-scanner-types";
 export { UsbScannerOptions } from "./scanner/options.interface";
 export { PathOption } from "./scanner/options.interface";
 export { VendorProductOption } from "./scanner/options.interface";
 export { IHidMap } from "./hid-map/hid-map.interface";
-export { getDevices, getDevice, getDeviceByPath } from './devices/device-helper-functions';
+export { HidMap } from "./hid-map/hid-map";
+export { getDevices, getDevice, getDeviceByPath } from "./devices/device-helper-functions";
